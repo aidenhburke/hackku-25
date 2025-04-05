1f29a2b342c973c4a092a7b2e105c2322289341e
Aiden Burke <aidenburke@ku.edu>
2025-04-05 13:19:11 -0500

    update dataset

--- algorithm_refining/main.py
+++ algorithm_refining/main.py
@@ -14,36 +14,47 @@ def main():
         with open('input_files/' + input_file, 'r') as file:
             data = json.load(file)
 
-        start_time = data[0]['timestamp']
-        for entry in data:
-            entry['timestamp'] -= start_time
-
         time = np.array([entry['timestamp'] for entry in data])
         x = np.array([entry['x'] for entry in data])
         y = np.array([entry['y'] for entry in data])
         z = np.array([entry['z'] for entry in data])
 
         norm = np.sqrt(x**2 + y**2 + z**2)
+        max = 0
+        max_i = 0
+        for i in range(len(norm)):
+            if norm[i] > max:
+                max = norm[i]
+                max_i = i
+        
+        normalized_time = time[max_i]
+        # Normalize time to start from 0
+        time = time - normalized_time
 
         # Filter duplicate timestamps
         valid_indices = np.where(np.diff(time, prepend=time[0] - 1) > 1e-6)[0]
         time = time[valid_indices]
         norm = norm[valid_indices]
+        time, norm = time[5:], norm[5:]  # Skip first 5 samples
 
         da = np.gradient(norm, time)
 
         # Plot individual file
         fig, axs = plt.subplots(2, 1, figsize=(10, 10), sharex=True)
         axs[0].plot(time, norm, color='k')
+        axs[0].set_ylim(0, 150)
+        axs[0].set_xlim(-5000, 5000)
         axs[0].set_title('Overall Acceleration vs Time')
         axs[0].set_ylabel('Overall Acceleration')
-        axs[0].set_xlabel('Time')
+        axs[0].set_xlabel('Time (ms)')
         axs[0].grid(True)
 
         axs[1].plot(time, da, color='m')
+        axs[1].set_ylim(-7, 7)
+        axs[1].set_xlim(-5000, 5000)
         axs[1].set_title('Derivative of Overall Acceleration vs Time')
         axs[1].set_ylabel('dA/dt (Jerk)')
-        axs[1].set_xlabel('Time')
+        axs[1].set_xlabel('Time (ms)')
         axs[1].grid(True)
 
         plt.tight_layout()
@@ -71,13 +82,20 @@ def main():
         plt.scatter(nonfall_times, nonfall_values, color='blue', label='Non-Falls', alpha=0.6)
 
     plt.title('Acceleration Extrema from All Files')
-    plt.xlabel('Time (s)')
+    plt.xlabel('Time (ms)')
     plt.ylabel('Acceleration')
+    plt.xlim(-5000, 5000)
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
     plt.savefig('output_files/all_extrema_summary.png')
     plt.show()
+
+    '''
+    change the figures to instaed of displaying the entire sensor data, isolate the highest point of acceleration
+    which is the global max and then plot that point in the center at t = 0 and the the rest either around it or 
+    only 4000 ms in either direction
+    '''
 
 if __name__ == '__main__':
     main()
