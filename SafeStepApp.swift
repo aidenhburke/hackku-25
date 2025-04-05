673ae0eab7ccdd6a50424165917e8338077bc919
Ty Farrington <155844627+TyFarrington@users.noreply.github.com>
2025-04-05 00:15:45 -0500

    Add files via upload
    Essentially the Main so Far

---
+++ SafeStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeStepApp.swift
+//  SafeStep
+//
+//  Created by Ty Farrington on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SafeStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
