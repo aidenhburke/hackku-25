80fe311f31a33bbf0468d4efd1e23e08e88643dd
marktmaloney <markmaloney03@yahoo.com>
2025-04-05 20:54:54 -0500

    changes from twilio to nexmo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express"); //Express for building API
 const cors = require("cors");
 const dotenv = require("dotenv"); //Load environmental variables
-const twilio = require("twilio"); //Twilio for sedning messages
+const { Vonage } = require("@vonage/server-sdk"); // Vonage for sending messages
 
 dotenv.config(); //Load env vars
 
@@ -10,11 +10,11 @@ const app = express();
 app.use(cors()); //Allows requests froms swift
 app.use(express.json()); // To parse incoming JSON requests
 
-//Initialize Twilio with credentials from env file
-const client = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
+// Initialize Vonage client with credentials from env file
+const vonage = new Vonage({
+  apiKey: process.env.VONAGE_API_KEY,
+  apiSecret: process.env.VONAGE_API_SECRET,
+});
 
 // API Route to send SMS
 app.post("/api/send_sms", async (req, res) => {
@@ -29,16 +29,34 @@ app.post("/api/send_sms", async (req, res) => {
   const message = `A fall has been detected from ${name}'s phone. Location: ${location}`;
 
   try {
-    //Send messages to all provided numbers
-    const sendPromises = numbers.map((number) =>
-      client.messages.create({
-        body: message,
-        from: process.env.TWILIO_PHONE_NUMBER,
-        to: number,
-      })
+    // Send messages to all provided numbers
+    const sendPromises = numbers.map(
+      (number) =>
+        new Promise((resolve, reject) => {
+          vonage.sms.send(
+            {
+              from: "SafeStep", // Can be your app name or phone number
+              to: number,
+              text: message,
+            },
+            (error, responseData) => {
+              if (error) {
+                reject(error);
+              } else if (responseData.messages[0].status !== "0") {
+                reject(
+                  new Error(
+                    `Message failed with error: ${responseData.messages[0]["error-text"]}`
+                  )
+                );
+              } else {
+                resolve(responseData.messages[0].messageId);
+              }
+            }
+          );
+        })
     );
 
-    await Promise.all(sendPromises); //Wait for messgaes to be sent
+    await Promise.all(sendPromises); // Wait for all messages to be sent
 
     res.status(200).json({ message: "Messages sent successfully!" });
   } catch (error) {
